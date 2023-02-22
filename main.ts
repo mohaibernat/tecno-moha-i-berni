836d03dad318727a3f459c587fae19356863134d
mohaibernat <126066777+mohaibernat@users.noreply.github.com>
2023-02-22 11:03:40 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . # . .
+    `)
+basic.forever(function () {
+	
+})
